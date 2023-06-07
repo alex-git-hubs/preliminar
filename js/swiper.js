34ba5ae275681a8db95f502b227f71feb942a2f1
alex-git-hubs <alexg_22@outlook.com>
2023-06-06 21:26:57 -0430

    inclusion de carrusel

---
+++ js/swiper.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper(".swiper-hero", {
+  // Optional parameters
+  slidesPerView: "auto",
+  spaceBetween: 5,
+  slidespergroupauto: false,
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
+  autoHeight: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets", // por defecto es bullest
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
